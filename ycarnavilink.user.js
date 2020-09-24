3caa6e8b2de79b4f54374d0bd686bc2bb66e8525
AG739107 <59082337+AG739107@users.noreply.github.com>
2020-09-24 20:05:08 +0900

    Create ycarnavilink.user.js

---
+++ ycarnavilink.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @id             iitc-plugin-yahooCarNaviLink
+// @name           IITC plugin: yahoo CarNavi Link
+// @category       Misc
+// @version        0.0.1.20200924.200000
+// @description    none
+// @updateURL      https://raw.githubusercontent.com/AG739107/iitc-plugins/master/ycarnavilink.user.js
+// @downloadURL    https://raw.githubusercontent.com/AG739107/iitc-plugins/master/ycarnavilink.user.js
+// @namespace      https://raw.githubusercontent.com/AG739107/iitc-plugins
+// @include        https://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
+
+/* globals $ */
+
+function wrapper(plugin_info) {
+  // Ensure plugin framework is there, even if iitc is not yet loaded
+  if (typeof window.plugin !== 'function') window.plugin = function () {};
+
+  // PLUGIN START //////////////////////////////////////////////////////////
+
+  // PLUGIN END //////////////////////////////////////////////////////////
+
+  // Add the script info data to the function as a property
+  setup.info = plugin_info;
+  if (!window.bootPlugins) window.bootPlugins = [];
+  window.bootPlugins.push(setup);
+
+  // If IITC has already booted, immediately run the 'setup' function
+  if (window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+
+// Inject code into site context
+var script = document.createElement('script');
+var info = {};
+
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
+  info.script = {
+    version: GM_info.script.version,
+    name: GM_info.script.name,
+    description: GM_info.script.description
+  };
+}
+
+script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(info) + ');'));
+(document.body || document.head || document.documentElement).appendChild(script);
