ee70dbb5ad7e214606e18a46bd9f8ac51c1b69c8
AG739107 <59082337+AG739107@users.noreply.github.com>
2020-09-25 13:21:32 +0900

    Update ycarnavilink.user.js

--- ycarnavilink.user.js
+++ ycarnavilink.user.js
@@ -20,15 +20,52 @@ function wrapper(plugin_info) {
 
   // PLUGIN START //////////////////////////////////////////////////////////
 
-  // Use own namespace for plugin
-  window.plugin.ycarnavi = function () {};
+  window.plugin.aurora.setupCSS = function () {
+    $('<style>')
+      .prop('type', 'text/css')
+      .html('#aurora-container {'
+          + 'display: block;'
+          + 'text-align: center;'
+          + 'margin: 6px 3px 1px 3px;'
+          + 'padding: 0 4px;}'
+        + '.aurora-container label {'
+          + 'white-space: nowrap;'
+          + 'margin: 0 0.3em;}'
+        + '.aurora-container input {'
+          + 'vertical-align: middle;}'
+        + '#aurora-portals {'
+          + 'border-collapse: collapse;'
+          + 'empty-cells: show;'
+          + 'width: 100%;}'
+        + '#aurora-portals th, #aurora-portals td {'
+          + 'padding: 3px; color: white;'
+          + ' background-color: #1b415e;'
+          + ' border-bottom: 1px solid #0b314e;}')
+      .appendTo('head');
+  };
 
-  var linkDetails2 = [];
-  var permalinkUrl2 = '/intel?ll='+lat+','+lng+'&z=17&pll='+lat+','+lng;
+  window.plugin.aurora.setupContent = function () {
+    window.plugin.aurora.contentHTML = '<div class="aurora-container" id="aurora-container">Aurora '
+      + '<label><input type="checkbox" id="glyph_1" onclick="window.plugin.aurora.updateGlyphed($(this).prop(\'checked\'), 1)"> 1</label>'
+      + '<label><input type="checkbox" id="glyph_2" onclick="window.plugin.aurora.updateGlyphed($(this).prop(\'checked\'), 2)"> 2</label>'
+      + '<label><input type="checkbox" id="glyph_3" onclick="window.plugin.aurora.updateGlyphed($(this).prop(\'checked\'), 3)"> 3</label>'
+      + '<label><input type="checkbox" id="glyph_4" onclick="window.plugin.aurora.updateGlyphed($(this).prop(\'checked\'), 4)"> 4</label>'
+      + '<label><input type="checkbox" id="glyph_5" onclick="window.plugin.aurora.updateGlyphed($(this).prop(\'checked\'), 5)"> 5</label>'
+      + '<label><input type="checkbox" id="glyph_6" onclick="window.plugin.aurora.updateGlyphed($(this).prop(\'checked\'), 6)"> 6</label>'
+      + '<label><input type="checkbox" id="glyph_7" onclick="window.plugin.aurora.updateGlyphed($(this).prop(\'checked\'), 7)"> 7</label>'
+      + '<label><input type="checkbox" id="glyph_8" onclick="window.plugin.aurora.updateGlyphed($(this).prop(\'checked\'), 8)"> 8</label>'
+      + '</div>';
 
-  // non-android - a permalink for the portal
-  var permaHtml2 = $('<div>').html( $('<a>').attr({href:permalinkUrl, title:'Create a URL link to this portal'}).text('ポータルへリンク2') ).html();
-  linkDetails2.push ( '<aside>'+permaHtml+'</aside>' );
+    window.plugin.aurora.disabledMessage = '<div id="aurora-container" class="help" title="Your browser does not support localStorage">Plugin Aurora disabled</div>';
+
+    $("#toolbox").append('<a onclick="window.plugin.aurora.showList();" title="Show portals for Aurora Glyph Hack Challenge">Aurora portals</a>');
+  };
+
+    var setup = function () {
+    window.plugin.aurora.setupCSS();
+    window.plugin.aurora.setupContent();
+  };
+
 
   // PLUGIN END //////////////////////////////////////////////////////////
 
