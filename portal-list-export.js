f0f2727600be6f50177800aa57afc536ddb433b4
AG739107 <59082337+AG739107@users.noreply.github.com>
2021-02-19 16:56:21 +0900

    Update portal-list-export.js

--- portal-list-export.js
+++ portal-list-export.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-portal-list-export
 // @name           IITC plugin: ingress Portal List Exporter
 // @category       Misc
-// @version        0.0.1.20210216.210000
+// @version        0.0.1.20210219.1165500
 // @namespace      Portal-Export-CSV
 // @updateURL      https://raw.githubusercontent.com/AG739107/iitc-plugins/master/portal-list-export.js
 // @downloadURL    https://raw.githubusercontent.com/AG739107/iitc-plugins/master/portal-list-export.js
-// @description    [iitc-2021-02-16-210000] This is the first release.
+// @description    [iitc-2021-02-19-165500] This is the first release.
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
@@ -27,18 +27,18 @@ function wrapper() {
     window.plugin.ingressdualmap = function() {};
     var self = window.plugin.ingressdualmap;
     self.gen = function gen() {
-        var o = []; 
-        for (var x in window.portals) {
-            var p = window.portals[x];
+        var o = [];
+        for (var guid in window.portals) {
+            var p = window.portals[guid];
             var b = window.map.getBounds();
             // skip if not currently visible
             if (p._latlng.lat < b._southWest.lat || p._latlng.lng < b._southWest.lng
                 || p._latlng.lat > b._northEast.lat || p._latlng.lng > b._northEast.lng) continue;
-            o.push("\"" + p.options.data.title.replace(/\"/g, "\\\"") + "\"," + p._latlng.lat + "," + p._latlng.lng);
+            o.push("\"" + p.options.data.title.replace(/\"/g, "\\\"") + "\"," + p._latlng.lat + "," + p._latlng.lng + "," + guid);
         }
         var dialog = window.dialog({
-            title: "Ingress Dual Map: CSV export",
-            html: '<span>Save the list below as a CSV file, then copy it to <code>/sdcard/IngressDualMap</code> on your phone.</span>'
+            title: "CSV export",
+            html: '<span>Save the list below as a CSV file, then copy it.</span>'
             + '<textarea id="idmCSVExport" style="width: 600px; height: ' + ($(window).height() - 150) + 'px; margin-top: 5px;"></textarea>'
         }).parent();
         $(".ui-dialog-buttonpane", dialog).remove();
@@ -53,7 +53,7 @@ function wrapper() {
     // setup function called by IITC
     self.setup = function init() {
         // add controls to toolbox
-        var link = $("<a onclick=\"window.plugin.ingressdualmap.gen();\" title=\"Generate a CSV list of portals and locations for use with Ingress Dual Map.\">IDM Export</a>");
+        var link = $("<a onclick=\"window.plugin.ingressdualmap.gen();\" title=\"Generate a CSV list of portals and locations.\">Export Portal List</a>");
         $("#toolbox").append(link);
         // delete setup to ensure init can't be run again
         delete self.setup;
